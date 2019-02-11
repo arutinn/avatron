c611c3837b8bd358520e33ff6eddb02fa62bb19a
arutinn <slavik_arutin@rambler.ru>
2019-02-11 15:01:32 +0200

    new mailgun credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,13 +63,13 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "avatron_#{Rails.env}"
 
-  config.action_mailer.default_url_options = { :host => 'avatron.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'avatron.pp.ua' }
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
 
-  # config.action_mailer.delivery_method = :smtp
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   
   config.action_mailer.smtp_settings = {
